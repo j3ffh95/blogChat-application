59b9ace3c3c77ff85d9cd1a48ac89e68110674a8
Jefferson Huisa <j3ffh95@gmail.com>
2021-11-16 09:22:03 -0500

    Created views folder

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Welcme to our new app");
+  res.send("Welcome to our new app");
 });
 
 app.listen(3000);
