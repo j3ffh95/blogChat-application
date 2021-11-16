dcd9dda38137c43e1b586edf35313279536972e2
Jefferson Huisa <j3ffh95@gmail.com>
2021-11-16 09:27:47 -0500

    Set up express template system

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 
 // Set express views so it could know where to look for it (in the views folder)
 app.set("views", "views");
+// let express know which template system/engine we are using
+app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
   res.send("Welcome to our new app");
