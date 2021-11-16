93e15698422201e6028ffe43fdcc6868ceaab3bb
Jefferson Huisa <j3ffh95@gmail.com>
2021-11-16 09:34:28 -0500

    Created public folder to put all files accesable to user

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+// Let express know we are using the public folder
+app.use(express.static("public"));
 // Set express views so it could know where to look for it (in the views folder)
 app.set("views", "views");
 // let express know which template system/engine we are using
