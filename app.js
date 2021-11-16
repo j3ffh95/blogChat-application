a1dcf1fa95b56b15de2b9a44ac367d164468ee64
Jefferson Huisa <j3ffh95@gmail.com>
2021-11-16 09:26:10 -0500

    Set up on where to get the views folder

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+// Set express views so it could know where to look for it (in the views folder)
+app.set("views", "views");
+
 app.get("/", function (req, res) {
   res.send("Welcome to our new app");
 });
