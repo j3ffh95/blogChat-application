f9cb16854b4a751bf02dd76a788ee56266ebdf8f
Jefferson Huisa <j3ffh95@gmail.com>
2021-11-17 20:04:01 -0500

    Required the user controller file to router file

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 // It is the routers job to list out all of the URL's routes that we are on the lookout for
 const express = require("express");
 const router = express.Router();
+const userController = require("./controllers/userController");
 
 router.get("/", function (req, res) {
   res.render("home-guest");
