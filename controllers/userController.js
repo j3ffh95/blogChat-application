d1f1a7ba765cd0ce9ef552d3aa17dddaefafeb32
Jefferson Huisa <j3ffh95@gmail.com>
2021-11-17 20:34:52 -0500

    called user register function

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,7 @@ exports.login = function () {};
 exports.logout = function () {};
 exports.register = function (req, res) {
   let user = new User(req.body);
+  user.register();
   res.send("thanks for registering");
 };
 exports.home = function (req, res) {
