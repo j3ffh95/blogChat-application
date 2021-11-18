72a46a65e58b15d8b7ef11be987b85c564cc86f0
Jefferson Huisa <j3ffh95@gmail.com>
2021-11-17 20:19:45 -0500

    made our app use html form submit and json data

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = require("./router");
 const app = express();
 
+// HTML Form submit - let express know to add the user submitted data onto our request object, so then we can access it from request.body
+app.use(express.urlencoded({ extended: false }));
+// Let express know about sending Json data
+app.use(express.json());
+
 // Let express know we are using the public folder
 app.use(express.static("public"));
 // Set express views so it could know where to look for it (in the views folder)
