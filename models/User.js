b722c094ca57e8f9ce6c31f3209a32fd057df650
Jefferson Huisa <j3ffh95@gmail.com>
2021-11-18 10:34:08 -0500

    Added validation if the fields are empty

--- models/User.js
+++ models/User.js
@@ -1,9 +1,20 @@
 // This is our constructor function (Blueprint)
 let User = function (data) {
   this.data = data;
+  this.errors = [];
 };
 
-User.prototype.validate = function () {};
+User.prototype.validate = function () {
+  if (this.data.username == "") {
+    this.errors.push("You must provide a username.");
+  }
+  if (this.data.email == "") {
+    this.errors.push("You must provide a valid email address.");
+  }
+  if (this.data.password == "") {
+    this.errors.push("You must provide a password.");
+  }
+};
 
 User.prototype.register = function () {
   // Step 1: Validate user data
