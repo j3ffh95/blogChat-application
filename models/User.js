04ae8288dc6d127d95aff51cf6ef714f6312996b
Jefferson Huisa <j3ffh95@gmail.com>
2021-11-18 10:29:32 -0500

    Added comments to know what to do

--- models/User.js
+++ models/User.js
@@ -3,6 +3,9 @@ let User = function (data) {
   this.data = data;
 };
 
-User.prototype.register = function () {};
+User.prototype.register = function () {
+  // Step 1: Validate user data
+  // Step 2: Only if there are no vaildation errors then save the user data into a database
+};
 
 module.exports = User;
