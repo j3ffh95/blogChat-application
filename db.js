9d49ca79312d16ed2957702eeb6894ecac814de2
Jefferson Huisa <j3ffh95@gmail.com>
2021-11-20 00:03:16 -0500

    made the app listen as soon as we have a connection with the db

--- db.js
+++ db.js
@@ -8,5 +8,7 @@ mongodb.connect(
   { useNewUrlParser: true, useUnifiedTopology: true },
   function (err, client) {
     module.exports = client.db();
+    const app = require("./app");
+    app.listen(3000);
   }
 );
