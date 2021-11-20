cbb1175f4c117de4dd06c4d0e5294315190964d8
Jefferson Huisa <j3ffh95@gmail.com>
2021-11-19 23:57:39 -0500

    connection to mongodb

---
+++ db.js
@@ -0,0 +1,10 @@
+const mongodb = require("mongodb");
+
+const connectionString =
+  "mongodb+srv://j3ffh95:soccer1995@cluster0.ezsop.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongodb.connect(
+  connectionString,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (err, client) {}
+);
