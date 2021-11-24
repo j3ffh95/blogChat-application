236682fa8fc67f4af7d6afcf84eaccfba22f77d2
Jefferson Huisa <j3ffh95@gmail.com>
2021-11-23 22:26:50 -0500

    require in the dot env file

--- db.js
+++ db.js
@@ -1,7 +1,5 @@
+const dotenv = require("dotenv");
 const mongodb = require("mongodb");
-
-const connectionString =
-  "mongodb+srv://j3ffh95:soccer1995@cluster0.ezsop.mongodb.net/WhatsGood?retryWrites=true&w=majority";
 
 mongodb.connect(
   connectionString,
